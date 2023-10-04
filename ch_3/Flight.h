1fc00be4823ee30f819a07c288690b0954d008c5
acarston <amcarston@gmail.com>
2023-10-03 23:22:13 -0600

    add FindPassengerIndex function

--- ch_3/Flight.h
+++ ch_3/Flight.h
@@ -14,6 +14,7 @@ class Flight {
 
         void AddPassenger(std::string name) { this->passengers.orderInsert(name); };
         void RemovePassenger(std::string name) { this->passengers.removeFirst(name); };
+        int FindPassengerIndex(std::string name) { return this->passengers.findFirst(name); };
         void PrintPassengers() { this->passengers.out(); };
     private:
         int id;
