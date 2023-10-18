5fc7160ff35fd36b94369fa302808c7219b94c19
acarston <amcarston@gmail.com>
2023-10-18 15:23:07 -0600

    change formatting

--- utils/Stack.h
+++ utils/Stack.h
@@ -3,14 +3,14 @@
 
 #include "SLLNode.h"
 
-/* since this is a template class, everything is in the dot h file. 
-according to _The C++ standard library: a tutorial and handbook_: 
-"the only portable way of using templates at the moment is to implement 
-them in header files by using inline functions." also, there is no error
-handling for calling the functions when the stack is empty. it is up to 
-the programmer to avoid seg faults using the 'empty' function. this is a 
-performance choice, similar to how arrays don't throw 'index out of 
-bounds' errors. */
+// since this is a template class, everything is in the dot h file. 
+// according to _The C++ standard library: a tutorial and handbook_: 
+// "the only portable way of using templates at the moment is to implement 
+// them in header files by using inline functions." also, there is no error
+// handling for calling the functions when the stack is empty. it is up to 
+// the programmer to avoid seg faults using the 'empty' function. this is a 
+// performance choice, not dissimilar to how arrays don't throw 'index out 
+// of bounds' errors.
 
 template <typename T>
 class Stack {
