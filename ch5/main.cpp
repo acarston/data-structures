9c758c8c372744e2fbf73ed2e8e7891178372e55
acarston <amcarston@gmail.com>
2023-10-22 15:23:36 -0600

    remove stack overflow handling

--- ch5/main.cpp
+++ ch5/main.cpp
@@ -1,5 +1,4 @@
 #include "rastarr.h"
-// #include <sys/resource.h>
 
 void check_args(int argc) {
     if (argc < 2) {
@@ -13,24 +12,6 @@ void check_args(int argc) {
 };
 
 int main(int argc, char* argv[]) {
-    // const rlim_t kStackSize = 1024 * 1024 * 1024;   // min stack size = 16 MB
-    // struct rlimit rl;
-    // int result;
-
-    // result = getrlimit(RLIMIT_STACK, &rl);
-    // if (result == 0)
-    // {
-    //     if (rl.rlim_cur < kStackSize)
-    //     {
-    //         rl.rlim_cur = kStackSize;
-    //         result = setrlimit(RLIMIT_STACK, &rl);
-    //         if (result != 0)
-    //         {
-    //             fprintf(stderr, "setrlimit returned result = %d\n", result);
-    //         }
-    //     }
-    // }
-
     check_args(argc);
 
     int rows, cols;
@@ -40,7 +21,4 @@ int main(int argc, char* argv[]) {
     int** map = rastarr::get_map(arr, rows, cols);
     if (argc == 2) rastarr::output_map(map, rows, cols);
     else rastarr::output_map(map, rows, cols, argv[2]);
-
-    // if (argc == 2) rastarr::print_stands(arr, rows, cols);
-    // else rastarr::print_stands(arr, rows, cols, argv[2]);
 };
