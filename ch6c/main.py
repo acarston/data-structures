a59b323a874b00036404aa4ef495f0966d599a48
acarston <amcarston@gmail.com>
2023-11-17 20:44:19 -0700

    restructure files

--- ch6c/main.py
+++ ch6c/main.py
@@ -8,15 +8,15 @@ def main():
     os.chdir(os.path.dirname(__file__))
 
     textFile = TextFile()
-    with open('./foo.csv') as csvfile:
+    with open('./data/foo.csv') as csvfile:
         reader = csv.reader(csvfile)
         for row in reader:
             textFile.set_input(row[1], row[0])
             textFile.parse_into_tree()
-        textFile.print_words('./out.csv')
+        textFile.print_words('./data/out.csv')
     
     #json dumping by Timothy Zink
-    with open('./out.csv') as csvfile:
+    with open('./data/out.csv') as csvfile:
         reader = csv.reader(csvfile)
         data: list[dict[str, str | list[str] | int]] = [{
             "word": row[0],
@@ -24,7 +24,7 @@ def main():
             "count": row[2]
         } for row in reader]
 
-        json.dump(data, open("./data.json", "w"))
+        json.dump(data, open("./data/data.json", "w"))
 
 if __name__ == "__main__":
     main()
