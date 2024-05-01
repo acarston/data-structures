74f0ae968281894ca10d39d04d286485e7842601
acarston <98997002+acarston@users.noreply.github.com>
2024-05-01 16:23:49 -0600

    Update EventParticipant.cpp

--- ch1/EventParticipant.cpp
+++ ch1/EventParticipant.cpp
@@ -2,20 +2,20 @@
 
 #include "EventParticipant.h"
 
-void EventParticipant::setName(const std::string& name) { this->name = name; };
-void EventParticipant::setSex(const std::string& sex) { this->sex = sex; };
-void EventParticipant::setTeam(const std::string& team) { this->team = team; };
-void EventParticipant::setAge(const std::string& age) { this->age = age; };
-void EventParticipant::setPlace(int place) { this->place = place; };
-void EventParticipant::setPoints(int points) { this->points = points; };
+void EventParticipant::setName(const std::string& name) { this->name = name; }
+void EventParticipant::setSex(const std::string& sex) { this->sex = sex; }
+void EventParticipant::setTeam(const std::string& team) { this->team = team; }
+void EventParticipant::setAge(const std::string& age) { this->age = age; }
+void EventParticipant::setPlace(int place) { this->place = place; }
+void EventParticipant::setPoints(int points) { this->points = points; }
 
-std::string EventParticipant::getName() { return name; };
-std::string EventParticipant::getSex() { return sex; };
-std::string EventParticipant::getTeam() { return team; };
-std::string EventParticipant::getAge() { return age; };
-int EventParticipant::getPlace() { return place; };
-int EventParticipant::getPoints() { return points; };
+std::string EventParticipant::getName() { return name; }
+std::string EventParticipant::getSex() { return sex; }
+std::string EventParticipant::getTeam() { return team; }
+std::string EventParticipant::getAge() { return age; }
+int EventParticipant::getPlace() { return place; }
+int EventParticipant::getPoints() { return points; }
 
-/* implemented by children */
+// implemented by children
 void EventParticipant::setResult(std::string result) { return; };
 std::string EventParticipant::getResult() { return NULL; };
